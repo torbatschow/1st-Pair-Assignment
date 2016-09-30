19ea02e3b970736a53b3380c4c38eb61011b88c0
Alex <ohne.realitaet@posteo.de>
2016-09-30 14:44:18 +0200

    Create getDataAlex.R

---
+++ 1stAssignment/getDataAlex.R
@@ -0,0 +1,14 @@
+############
+# R-File Get and Clean Data for Assignment 1
+# Torben and Alexander
+# Updated September 30, 2016
+# Formatted according to Google style guide
+###########
+
+#Load packages
+
+#Set Working Directory
+
+#Get Data
+
+#Clean Data
