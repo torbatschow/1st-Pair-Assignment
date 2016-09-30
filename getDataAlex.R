77d3a6776ab67e9f21da4d274737c7faf59fec5f
Corrod3 <ohne.realitaet@posteo.de>
2016-09-30 15:44:46 +0200

    urls code

--- getDataAlex.R
+++ getDataAlex.R
@@ -6,12 +6,16 @@
 ###########
 
 #Load packages
+library(RCurl) #needed to access https from github, see http://stackoverflow.com/questions/14441729/read-a-csv-from-github-into-r
 
 #Set Working Directory
-try(setwd("D:\Eigene Datein\Dokumente\Uni\Hertie\Materials\Collaborative Social Science Data Analysis\1st-Pair-Assignment"), silent = TRUE)
+try(setwd("D:/Eigene Datein/Dokumente/Uni/Hertie/Materials/Collaborative Social Science Data Analysis/1st-Pair-Assignment"), silent = TRUE)
 try(setwd("C:/Users/Lisa/Documents/GitHub/CSSR_FinalProject"), silent = TRUE)
 
 #Get Data
+
+x <- getURL("https://raw.github.com/aronlindberg/latent_growth_classes/master/LGC_data.csv")
+y <- read.csv(text = x)
 #link: https://github.com/fivethirtyeight/data
 
 #Clean Data
