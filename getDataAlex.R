ff1a9b1fd72d4eb4d44fe2961ee278d574a1bd9d
torbatschow <mail@torbenklausa.de>
2016-09-30 16:09:45 +0200

    minor format update

--- getDataAlex.R
+++ getDataAlex.R
@@ -2,7 +2,6 @@
 # R-File Get and Clean Data for Assignment 1
 # Torben and Alexander
 # Updated September 30, 2016
-# Formatted according to Google style guide
 ###########
 
 #Load packages/library
