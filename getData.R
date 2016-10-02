a1d8266df09455990540ad97cdb9dc611aeedcd8
torbatschow <mail@torbenklausa.de>
2016-10-02 10:42:52 +0200

    install w/ dependencies

--- getData.R
+++ getData.R
@@ -15,7 +15,7 @@ packages <- c("RCurl")
 for (p in packages) {
   if (p %in% installed.packages()[,1]) require(p, character.only=T)
   else {
-    install.packages(p)
+    install.packages(p, dependencies = TRUE)
   }
   require(p, character.only=T)
 }
