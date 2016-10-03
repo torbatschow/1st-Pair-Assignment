9671cdbcbcca888601de43652dcf0c12b47c5773
Alex <ohne.realitaet@posteo.de>
2016-10-03 22:52:53 +0200

    copy paste vom rechner

--- stats+Graphics.R
+++ stats+Graphics.R
@@ -4,26 +4,50 @@
 # Updated September 30, 2016
 ###########
 
+#####################
+# Preparations
+#####################
+packages <- c("plyr", "ggplot2", "vcd")
+
+#install packages if not installed before
+for (p in packages) {
+  if (p %in% installed.packages()[,1]) require(p, character.only=T)
+  else {
+    install.packages(p)
+  }
+  require(p, character.only=T)
+}
+
+library(plyr) #basic functions like count
+library(ggplot2) #package for graphs
+library(vcd) # for mosaic chart
+
+#####################
+# Data presentation
+#####################
+
+# 1. Data preparation
+#list of factors
+cause.level <- levels(policeKilling$cause)
+#count the causes
+cause.count <- count(policeKilling, "cause")[,2]
+#put them together in a df
+cause.df <- data.frame(cause.level, cause.count)
+
+#2. Charts
+
+# Simple Bar Plot (from R) - not used in assignment
+cause.tab <- table(policeKilling$cause)
+barplot(cause.tab, main="Number of death by cause",
+        xlab="Cause")
 
 
-#####
-#Data presentation
-####
-
-# Idea: Barplot of number of killed on y axis and ethnicity on x axis
-
-#killEthnicity <- barplot(policeKilling$raceethnicity,
-#              main = "Ethnicity of individuals killed by policy",
-#             names.arg = c("Black","Brown","Red","Blonde"),
-#              ylab = "Absolute number",
-#              ylim = c(0,130),
-#              col = c("black", "brown", "red", "yellow")
-# )
-# Adding actual numbers on the bars
-#text(killEthnicity,HairEyeColorTable[,1]-4, 
-#     labels=HairEyeColorTable[,1], 
-#     cex = 1, 
-#     adj = c(1,-1))
+# Bar plot with ggplot2 (good intro: http://www.sthda.com/english/wiki/ggplot2-barplots-quick-start-guide-r-software-and-data-visualization)
+cause.bar2 <- ggplot(data=cause.df, aes(x=cause.level, y=cause.count)) + 
+  geom_bar(stat="identity", fill="steelblue") +
+  theme_minimal() +
+  labs(title="Number of killed by cause", 
+       x="Cause", y = "Number")
 
 
-# Idea: Mosaique Plot of cause of death and whether they are armed 
+
